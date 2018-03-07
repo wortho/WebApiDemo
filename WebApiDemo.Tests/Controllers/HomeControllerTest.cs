95ddc6b92a7b0d17ddb639103d982657cdf52bd9
David Worthington <CONSDAWO@widex.com>
2018-03-07 09:22:50 +0100

    Remove unused references in ASP.NET Web API Template.

--- WebApiDemo.Tests/Controllers/HomeControllerTest.cs
+++ WebApiDemo.Tests/Controllers/HomeControllerTest.cs
@@ -1,6 +1,5 @@
 ﻿using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using WebApiDemo;
 using WebApiDemo.Controllers;
 
 namespace WebApiDemo.Tests.Controllers
--- WebApiDemo.Tests/Controllers/ValuesControllerTest.cs
+++ WebApiDemo.Tests/Controllers/ValuesControllerTest.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
-using System.Text;
-using System.Web.Http;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using WebApiDemo;
 using WebApiDemo.Controllers;
 
 namespace WebApiDemo.Tests.Controllers
